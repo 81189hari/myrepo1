41ccb654d7f591eddb4415c2a99c3b524c2b6341
81189hari <81189harichandra@gmail.com>
2025-08-19 16:03:21 +0530

    Add files via upload

---
+++ hash.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class hash{
+   public static void main(String[] args)
+   {
+    HashMap <Integer,String> hash=new HashMap<>();
+	  hash.put(1,"ambition");
+	  hash.put(2," gallentry");
+	  hash.put(3,"perserverance");
+	  hash.put(4,"AGP");
+	  System.out.println("hash content is"+hash);
+	  System.out.println("value of  2 is "+ hash.get(2));
+	  }
+	  }
